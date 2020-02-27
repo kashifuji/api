7875fcadd9390bc8830e225a478ca57fbda197cc
kashifuji <katashiyo515@yahoo.co.jp>
2020-02-28 06:34:22 +0900

    Update dangerfile.ts

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,10 +1,10 @@
 import {message, danger} from "danger"
 
-# const modifiedMD = danger.git.modified_files.join("- ")
-# message("Changed Files in this PR: \n - " + modifiedMD)
+// # const modifiedMD = danger.git.modified_files.join("- ")
+// # message("Changed Files in this PR: \n - " + modifiedMD)
 
 warn("PR is classed as Work in Progress") if github.pr_title.include? "[WIP]"
 
-# ktlint
+// # ktlint
 checkstyle_format.base_path = Dir.pwd
 checkstyle_format.report 'app/build/reports/ktlint/ktlint-main.xml'
