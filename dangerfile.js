0f8893204286260641d3d1fbd0410d6ac2143f9d
kashifuji <katashiyo515@yahoo.co.jp>
2020-02-28 23:08:00 +0900

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -9,9 +9,20 @@ if (danger.github.pr.title.indexOf('[WIP]') === 0){
 const isMergeRefMaster = danger.github.pr.base.ref === 'master'; // master
 const isHeadRefDevelop = danger.github.pr.head.ref === 'develop'; // develop
 const isHeadRefHotfix  = danger.github.pr.head.ref.indexOf('hotfix/') === 0; // hotfix/xxx
-
 if (isMergeRefMaster && (!isHeadRefDevelop && !isHeadRefHotfix)) {
   fail("master should be merged from develop or hotfix")
+}
+
+import contains from "lodash-contains"
+import fs from "fs"
+
+const testFile = "target/ktlint.json"
+const linterOutput = fs.readFileSync(testFile).toString()
+
+if (contains(linterOutput, "Failed")) {
+  markdown(`These changes failed to pass the linter:
+${linterOutput}
+  `)
 }
 
 // const docs = danger.git.fileMatch("**/*.md")
