71736183971bf9c96135b6cde27559d6dca94402
kashifuji <katashiyo515@yahoo.co.jp>
2020-02-28 16:30:59 +0900

    Update dangerfile.ts

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,3 +1,17 @@
+// Import the feedback functions
+import { message, warn, fail, markdown } from "danger"
+
+// Add a message to the table
+message("You have added 2 more modules to the app")
+
+//  Adds a warning to the table
+warn("You have not included a CHANGELOG entry.")
+
+// Declares a blocking 
+fail(`ESLint has failed with ${fails} fails.`)
+
+// Show markdown under the table:
+markdown("## New module Danger" + dangerYarnInfo)
 //import {message, danger} from "danger"
 
 // # const modifiedMD = danger.git.modified_files.join("- ")
@@ -9,46 +23,46 @@
 // checkstyle_format.base_path = Dir.pwd
 // checkstyle_format.report "target/ktlint.xml"
 
-var globalFile = 'Rx.js';
-var minFile = 'Rx.min.js';
+// var globalFile = 'Rx.js';
+// var minFile = 'Rx.min.js';
 
-function sizeDiffBadge(name, value) {
-  var color = 'lightgrey';
-  if (value > 0) {
-    color = 'red';
-  } else if (value < 0) {
-    color = 'green';
-  }
-  return 'https://img.shields.io/badge/' + name + '-' + getFormattedKB(getKB(value)) + 'KB-' + color + '.svg?style=flat-square';
-}
+// function sizeDiffBadge(name, value) {
+//   var color = 'lightgrey';
+//   if (value > 0) {
+//     color = 'red';
+//   } else if (value < 0) {
+//     color = 'green';
+//   }
+//   return 'https://img.shields.io/badge/' + name + '-' + getFormattedKB(getKB(value)) + 'KB-' + color + '.svg?style=flat-square';
+// }
 
-//post size of build
-schedule(new Promise(function (res) {
-  getSize('./dist/cjs', function (e, result) {
-    var localGlobalFile = path.resolve('./dist/global', globalFile);
-    var localMinFile = path.resolve('./dist/global', minFile);
+// //post size of build
+// schedule(new Promise(function (res) {
+//   getSize('./dist/cjs', function (e, result) {
+//     var localGlobalFile = path.resolve('./dist/global', globalFile);
+//     var localMinFile = path.resolve('./dist/global', minFile);
 
-    //get sizes of PR build
-    var global = fs.statSync(localGlobalFile);
-    var global_gzip = gzipSize.sync(fs.readFileSync(localGlobalFile, 'utf8'));
-    var min = fs.statSync(localMinFile);
-    var min_gzip = gzipSize.sync(fs.readFileSync(localMinFile, 'utf8'));
+//     //get sizes of PR build
+//     var global = fs.statSync(localGlobalFile);
+//     var global_gzip = gzipSize.sync(fs.readFileSync(localGlobalFile, 'utf8'));
+//     var min = fs.statSync(localMinFile);
+//     var min_gzip = gzipSize.sync(fs.readFileSync(localMinFile, 'utf8'));
 
-    // [...]
+//     // [...]
 
-    var sizeMessage = '<img src="https://img.shields.io/badge/Size%20Diff%20%28' + releaseVersion + '%29--lightgrey.svg?style=flat-square"/>  ';
-    sizeMessage += '<img src="' + sizeDiffBadge('Global', global.size - bundleGlobal.size) + '"/> ';
-    sizeMessage += '<img src="' + sizeDiffBadge('Global(gzip)', global_gzip - bundle_global_gzip) + '"/> ';
-    sizeMessage += '<img src="' + sizeDiffBadge('Min', min.size - bundleMin.size) + '"/> ';
-    sizeMessage += '<img src="' + sizeDiffBadge('Min (gzip)', min_gzip - bundle_min_gzip) + '"/> ';
-    message(sizeMessage);
+//     var sizeMessage = '<img src="https://img.shields.io/badge/Size%20Diff%20%28' + releaseVersion + '%29--lightgrey.svg?style=flat-square"/>  ';
+//     sizeMessage += '<img src="' + sizeDiffBadge('Global', global.size - bundleGlobal.size) + '"/> ';
+//     sizeMessage += '<img src="' + sizeDiffBadge('Global(gzip)', global_gzip - bundle_global_gzip) + '"/> ';
+//     sizeMessage += '<img src="' + sizeDiffBadge('Min', min.size - bundleMin.size) + '"/> ';
+//     sizeMessage += '<img src="' + sizeDiffBadge('Min (gzip)', min_gzip - bundle_min_gzip) + '"/> ';
+//     message(sizeMessage);
 
-    markdown('> CJS: **' + getKB(result) +
-      '**KB, global: **' + getKB(global.size) +
-      '**KB (gzipped: **' + getKB(global_gzip) +
-      '**KB), min: **' + getKB(min.size) +
-      '**KB (gzipped: **' + getKB(min_gzip) + '**KB)');
+//     markdown('> CJS: **' + getKB(result) +
+//       '**KB, global: **' + getKB(global.size) +
+//       '**KB (gzipped: **' + getKB(global_gzip) +
+//       '**KB), min: **' + getKB(min.size) +
+//       '**KB (gzipped: **' + getKB(min_gzip) + '**KB)');
 
-    res();
-  });
-}));
+//     res();
+//   });
+// }));
