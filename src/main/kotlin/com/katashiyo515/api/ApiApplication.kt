20e037b505cd4bc853f880a2753ede64c18a644c
kashifuji <katashiyo515@yahoo.co.jp>
2020-03-05 09:05:17 +0900

    Update ApiApplication.kt

--- src/main/kotlin/com/katashiyo515/api/ApiApplication.kt
+++ src/main/kotlin/com/katashiyo515/api/ApiApplication.kt
@@ -6,6 +6,6 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class ApiApplication
 
-fun main(args: Array<String>) {
+fun main(args: Array<String>) {  
     runApplication<ApiApplication>(*args)
-}
+}   
