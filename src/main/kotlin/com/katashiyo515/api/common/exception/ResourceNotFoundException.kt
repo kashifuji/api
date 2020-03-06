a222f0d4605f3078b42e150253e9c2aa55aa7afe
kashifuji <katashiyo515@yahoo.co.jp>
2020-03-07 06:07:45 +0900

    added testcase (#19)

--- src/main/kotlin/com/katashiyo515/api/common/exception/ResourceNotFoundException.kt
+++ src/main/kotlin/com/katashiyo515/api/common/exception/ResourceNotFoundException.kt
@@ -3,7 +3,7 @@ package com.katashiyo515.api.common.exception
 import com.katashiyo515.api.common.error.ErrorCodeDefinition
 import com.katashiyo515.api.common.message.Message
 
-class ResourceNotFoundException(msg: String = "", private val id: Int? = null) : ApiRuntimeException(msg) {
+class ResourceNotFoundException(msg: String = "", val id: Int? = null) : ApiRuntimeException(msg) {
     override var detail: String = ""
         get() {
             return when (id) {
